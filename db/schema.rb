88e3ea5c1b557a9846e63356f50e7653269a4be8
sadmanahmed <abirsadmanahmed177@gmail.com>
2022-06-15 16:35:08 +0600

    feat: Vote model has been created which is belongs to both user & link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_15_100441) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_15_103252) do
   create_table "links", force: :cascade do |t|
     t.string "url"
     t.text "description"
@@ -28,5 +28,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_15_100441) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer "link_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
   add_foreign_key "links", "users"
+  add_foreign_key "votes", "links"
+  add_foreign_key "votes", "users"
 end
