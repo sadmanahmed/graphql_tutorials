0c9e76f63500f28c953093d5b989cac442effabb
sadmanahmed <abirsadmanahmed177@gmail.com>
2022-06-20 16:19:03 +0600

    feat: all_user and all_votes query added

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,8 +2,18 @@ module Types
   class QueryType < Types::BaseObject
     # field :all_links, [LinkType], null: false
     field :all_links, [LinkType], resolver: Resolvers::LinksSearch, null: false
+    field :all_users, [UserType], null: false
+    field :all_votes, [VoteType], null: false
     def all_links
       Link.all
+    end
+
+    def all_users
+      User.all
+    end
+
+    def all_votes
+      Vote.all
     end
   end
 end
