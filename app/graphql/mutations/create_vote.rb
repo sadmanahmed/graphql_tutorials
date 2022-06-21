5cdcd64fcc3b73447406ea1a0fd6df7abe993c69
sadmanahmed <abirsadmanahmed177@gmail.com>
2022-06-21 17:45:59 +0600

    feat: create vote mutation added

--- app/graphql/mutations/create_vote.rb
+++ app/graphql/mutations/create_vote.rb
@@ -1,13 +1,18 @@
 module Mutations
   class CreateVote < BaseMutation
     argument :link_id, ID, required: false
+    argument :user_id, ID, required: false
 
     type Types::VoteType
 
-    def resolve(link_id: nil)
+    def resolve(link_id: nil, user_id: nil)
       Vote.create!(
         link: Link.find(link_id),
-        user: context[:current_user]
+        # if user_id.nil?
+        user: User.find(user_id)
+        # else
+        #   user: context[:current_user]
+        # end
       )
     end
   end
